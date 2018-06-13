61f9618824442935dc901e727369b46f55b5571e
Nicholas Powers <njpowers7915@gmail.com>
2018-06-13 19:50:36 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,12 @@ class App < Sinatra::Base
     end
     
     post "/teams" do
+      @team = Team.new(params[:team])
+      params[:team][:heros].each do |details|
+        Hero.new(details)
+      end
+      
+      @heros = Hero.all
       
       erb :show
     end
